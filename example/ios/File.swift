d7012a445bb68a0c2602e779e8713ba1e6686680
Sabri <sabri.ghazali@satoripop.com>
2021-12-21 22:12:42 +0100

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  JitsiMeetSdkExample
+//
+
+import Foundation
---
+++ ios/JitsiMeetSdk.h
@@ -0,0 +1,5 @@
+#import <React/RCTBridgeModule.h>
+
+@interface JitsiMeetSdk : NSObject <RCTBridgeModule>
+
+@end
